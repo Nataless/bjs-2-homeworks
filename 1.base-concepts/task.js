fdd7fd0870f00b5dffab08401b267d85fa03fede
Natalie Voronova <Nataless@yandex.ru>
2024-01-22 15:40:31 +0300

    added Home work 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let x1;
+	let x2;
+	let d = b ** 2 - 4 * a * c;
+	if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 }
 
+console.log(solveEquation())
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
+
+	percent = percent / 100 / 12;
+
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+	let totalPayment = monthlyPayment * countMonths;
+
+	totalPayment = Number(totalPayment.toFixed(2));
+
+	return totalPayment;
 }
